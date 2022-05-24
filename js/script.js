14fc1d06af5d6b055018ce62624896ef9e8ad664
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2022-05-24 23:13:07 +0300

    last updates

--- js/script.js
+++ js/script.js
@@ -8,6 +8,8 @@
     var body = params.body || '';
     var success = params.success;
     var error = params.error;
+    // console.log('body', body);
+    // console.log('url', url);
 
     xhr.open('POST', url, true);
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
@@ -79,6 +81,13 @@
         'Required at least one number (0-9), uppercase and lowercase letters (a-Z) and at least one special character (!@#$%^&*-)'
       ),
     ],
+    password2: [
+      checkRegExp.bind(
+        null,
+        /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[\!\@\#\$\%\^\&\*\-])/,
+        'Required at least one number (0-9), uppercase and lowercase letters (a-Z) and at least one special character (!@#$%^&*-)'
+      ),
+    ],
     zip: [
       checkRegExp.bind(
         null,
@@ -92,9 +101,21 @@
     var fieldValidation = validations[element.id];
     var result = { valid: true, element: element, message: '' };
 
-    console.log('validations', fieldValidation);
+    if (element.id === 'password') {
+      localStorage.setItem('pass1', element.value);
+    }
+    if (element.id === 'password2') {
+      localStorage.setItem('pass2', element.value);
+    }
 
     if (fieldValidation) {
+      var pass1 = localStorage.getItem('pass1') || '';
+      var pass2 = localStorage.getItem('pass2') || '';
+
+      if (pass2 !== pass1 && element.id === 'password2') {
+        result.valid = false;
+        result.message = 'Must be to equal to password';
+      }
       for (var i = 0, len = fieldValidation.length; i < len; i++) {
         var validationFunction = fieldValidation[i];
 
@@ -107,7 +128,6 @@
         }
       }
     }
-    console.log('result', result);
     return result;
   }
 
@@ -124,20 +144,81 @@
     errorMessageElement && !message && (errorMessageElement.innerHTML = '');
   }
   function formOnchange(e) {
-    var firstPassword = document.getElementById('password');
-    var secondPassword = document.getElementById('password2');
-
     if (e.target.dataset && e.target.dataset.validation !== undefined) {
       toggleError(e.target, validateField(e.target).message);
     }
+  }
 
-    if (firstPassword.value !== secondPassword.value) {
-      toggleError(secondPassword, 'Passwords must be equle');
+  function nextBtnFunction(e) {
+    var counter = 0;
+    var allInputs = Array.from(
+      document.querySelectorAll('[data-validation]')
+    ).slice(0, -1);
+
+    var changeActiveStep = this.parentNode.parentNode.children[0].children;
+    var openHiddenBtn = this.parentNode.children;
+
+    for (var i = 0; i < allInputs.length; i++) {
+      var element = allInputs[i];
+      toggleError(element, validateField(element).message);
+      validateField(element).valid ? counter++ : counter--;
+
+      if (counter === allInputs.length) {
+        changeActiveStep[0].classList.remove('step_active');
+        changeActiveStep[1].classList.add('step_active');
+        helper(openHiddenBtn);
+      }
     }
   }
 
+  function prevBtnFunction() {
+    var prevBtn = this.parentNode.children;
+    var changeActiveStepBack = this.parentNode.parentNode.children[0].children;
+    changeActiveStepBack[1].classList.remove('step_active');
+    changeActiveStepBack[0].classList.add('step_active');
+
+    helper(prevBtn, changeActiveStepBack);
+  }
+
+  function helper(elements) {
+    for (var i = 0; i < elements.length; i++) {
+      const element = elements[i];
+      if (element.classList.contains('control_hide')) {
+        element.classList.remove('control_hide');
+      } else if (!element.classList.contains('control_hide')) {
+        element.classList.add('control_hide');
+      }
+    }
+  }
+
+  function checkZip(e) {
+    e.preventDefault();
+    let formToWorkOn = document.getElementById('mainForm');
+    const formData = new FormData();
+
+    formData.append('zip', document.getElementById('zip').value);
+
+    console.log('formData');
+    ajax({
+      url: '../api/geoStatus.php',
+      body: formData,
+      success: function (result) {
+        alert(result);
+      },
+      error: function (result) {
+        alert(result);
+      },
+    });
+  }
   /*
    * Listeners
    * */
   document.getElementById('mainForm').addEventListener('change', formOnchange);
+  document
+    .querySelector('[data-next]')
+    .addEventListener('click', nextBtnFunction);
+  document
+    .querySelector('[data-prev]')
+    .addEventListener('click', prevBtnFunction);
+  document.querySelector('[data-submit]').addEventListener('click', checkZip);
 })();
